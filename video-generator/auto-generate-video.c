5c296d811a5bb6f8d8342d83c15e87205ea83bcf
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2025-05-28 21:10:08 -0400

    Completed auto generate functionality

--- video-generator/auto-generate-video.c
+++ video-generator/auto-generate-video.c
@@ -1,38 +1,52 @@
+/*
+ * This file is used to auto generate videos.
+ * NOTE: The file containing the ideas must have different ideas on different lines, and must be a .txt file.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+#define BUFF_SIZE 1024
 
 
 #define OUT_FILE "./IO/ai-out"
 #define OUT_AUDIO "./IO/output.aiff"
 
 #define OUT_VIDEO "./videos/output.mp4"
-#define OUT_VIDEO_SUBS "./videos/output-with-subs.mp4"
+#define OUT_VIDEO_SUBS "./videos/output-with-subs" // Note: This is intentional, since we will append "i.mp4" for 1 <= i <= #lines in input file
 #define DEFAULT_VIDEO "./videos/mine_vid.mp4"
 
-// Uncomment to have audio file converted to wav or mp3:
-// #define AUDIO "-wav"
-// #define AUDIO "-mp3"
-#ifdef AUDIO
-#define DEFINED 1
-#else
-#define DEFINED 0
-// This will not affect the code since not defined, but necessary for compilation as the macro did not exist before
-#define AUDIO "Undefined" 
-#endif
-
-#define BUFF_SIZE 1028
-#define ANS_BUFF_SIZE 5000
-
-typedef char choice_t; // Either set to y or n
 
 char *IN_VIDEO;
+
+
+/*
+ * Function makes unique file names based on the number of files existing already/.
+ */
+char* make_filename(int i) {
+    // Determine required length dynamically
+    int needed = snprintf(NULL, 0, "%s%d.mp4", OUT_VIDEO_SUBS, i);
+    
+    // Allocate just enough space (+1 for null terminator)
+    char* filename = malloc(needed + 1);
+    if (!filename) {
+        perror("malloc failed");
+        exit(1);
+    }
+
+    // Build the final string
+    sprintf(filename, "%s%d.mp4", OUT_VIDEO_SUBS, i);
+
+    return filename;
+}
+
 
 /*
  * Function adds subtitles to produced video.
  */ 
-void add_subs() {
+void add_subs(char *output_with_subs_path) {
     pid_t pid_c;
     pid_c = fork();
     if (pid_c < 0) {
@@ -40,7 +54,7 @@ void add_subs() {
         exit(1);
     } else if (pid_c == 0) {
         printf("Adding subtitles...");
-        execl("./add-subs", "add-subs", OUT_VIDEO, OUT_VIDEO_SUBS, NULL);
+        execl("./add-subs", "add-subs", OUT_VIDEO, output_with_subs_path, NULL);
         perror("Execl failed. Subtitles unsuccessfully added");
         exit(1);
     } else {
@@ -102,11 +116,7 @@ void convert_prompt_to_audio() {
         perror("Fork failed");
         exit(1);
     } else if (pid_c == 0) {
-        if (DEFINED) {
-            execl("./text_to_audio", "text_to_audio", AUDIO, OUT_FILE, NULL);
-        } else {
-            execl("./text_to_audio", "text_to_audio", OUT_FILE, NULL);
-        }
+        execl("./text_to_audio", "text_to_audio", OUT_FILE, NULL);
         perror("Execl failed. Conversion to audio file unsuccessful.");
         exit(1);
     } else {
@@ -126,12 +136,14 @@ void convert_prompt_to_audio() {
 
 
 int main(int argc, char *argv[]) {
-    if (argc > 2) {
-        fprintf(stderr, "Proper usage:\n"
-                "./prompt-to-audio <background-video>\n"
-                "Choose video. Default will be the minecraft parkour video \n");    
+    if (argc > 3 || argc < 2) {
+        fprintf(stderr, 
+                "Error. Improper Usage!\n\n"
+                "Proper Usage: ./auto-generate-video <input-file.txt> <background-video.mp4>\n"
+                "Note: <background-video.mp4> is optional. If not defined, default video macro will be chosen.\n");
         exit(1);
-    } else if (argc == 1) {
+    }
+    else if (argc == 2) {
         fprintf(stdout, "Warning. Default usage requires user to define DEFAULT_VIDEO macro in current file.\n");
         IN_VIDEO = malloc(strlen(DEFAULT_VIDEO) + 1);
         if (IN_VIDEO == NULL) {
@@ -140,7 +152,7 @@ int main(int argc, char *argv[]) {
         }
         strcpy(IN_VIDEO, DEFAULT_VIDEO);
     } else {
-        IN_VIDEO = malloc(strlen(argv[1]) + 1);
+        IN_VIDEO = malloc(strlen(argv[2]) + 1);
         if (IN_VIDEO == NULL) {
             perror("Malloc failed");
             exit(1);
@@ -148,47 +160,57 @@ int main(int argc, char *argv[]) {
         strcpy(IN_VIDEO, argv[1]);
     }
 
-    int fd[2];
-    pid_t fork_pid;
-    choice_t choice = 'n'; // Default choice.
+    // Variables for logic
+    int num_lines = 0;
     char buff[BUFF_SIZE];
-    while (choice != 'y') {
-        printf("Enter video script prompt: \n");
-        while (fgets(buff, sizeof(buff), stdin) == 0) {
-            fprintf(stderr, "Enter valid prompt: \n");
-        }
+    FILE *file_ptr = fopen(argv[1], "r");
+    
 
+    if (file_ptr == NULL) {
+        perror("fopen failed.");
+        exit(1);
+    }
+
+    while (fgets(buff, BUFF_SIZE - 1, file_ptr) != NULL) {
+        int fd[2];
         if (pipe(fd) == -1) {
             perror("Pipe failed");
             exit(1);
         }
-
-        fork_pid = fork();
-        if (fork_pid < 0) {
+        pid_t pid = fork();
+        if (pid < 0) {
             perror("Fork failed");
             exit(1);
-        } else if (fork_pid == 0) {
+        } else if (pid == 0) {
             // Child process
             if (close(fd[1]) == -1) {
                 perror("Close failed");
                 exit(1);
             }
+            // Use dup2 to copy fd[0] read pipe file descriptor into stdin
             if (dup2(fd[0], fileno(stdin)) == -1) {
-                perror("Dupe2 failed");
+                perror("dup2 failed");
                 exit(1);
             }
+            // Since dup2 has been succesful, we can close read pipe
             if (close(fd[0]) == -1) {
                 perror("Close failed");
                 exit(1);
             }
+            // Run http-request to prompt AI. Note, that this program reads from stdin, hence explaining why we used dup2
             execl("./http-request", "http-request", NULL);
-            perror("Execl failed for http-request");
+
+            // If reached, execl failed
+            perror("Execl failed");
             exit(1);
         } else {
+            // Parent process
             if (close(fd[0]) == -1) {
                 perror("Close failed");
                 exit(1);
             }
+
+            // Write line idea to write end of the pipe
             if (write(fd[1], buff, strlen(buff)) == -1) {
                 perror("Write failed");
                 exit(1);
@@ -198,54 +220,34 @@ int main(int argc, char *argv[]) {
                 exit(1);
             }
 
-            int status;
+            pid_t status;
             if (wait(&status) == -1) {
                 perror("Wait failed");
                 exit(1);
             }
+
             if (WIFEXITED(status)) {
                 if (WEXITSTATUS(status) == 1) {
-                    perror("Error http request. Please verify if correct api key");
+                    perror("Error http request. Please verify if correct api key in Makefile");
                     exit(1);
-                } else {
-                    // Print contents of ai-out file
-                    char answer[ANS_BUFF_SIZE] = {'\0'};
-                    FILE *fp = fopen(OUT_FILE, "r");
-                    if (fp == NULL) {
-                        perror("fopen failed");
-                        exit(1);
-                    }
-                    size_t bytes_read;
-                    while ((bytes_read = fread(answer, 1, sizeof(buff), fp)) > 0) {
-                        printf("\nAI generated script:\n");
-                        fwrite(answer, 1, bytes_read, stdout);
-                    }
+                }
+                // Make output video file;
+                char *filename = make_filename(num_lines);
 
-                    // Check if we want to keep the answer, and convert
-                    printf("\n\n");
-                    printf("Convert answer to video or re-prompt or exit? [y/n/e]\n");
-                    while (fread(&choice, 1, 1, stdin) != 1) {
-                        printf("Convert answer to video or re-prompt or exit? [y/n/e]\n");
-                    }
-                    if (choice == 'e') {
-                        return 0;
-                    }
-                    if (choice != 'y') {
-                        // Makes sure that we only have y or n
-                        choice = 'n';
-                    } else {
-                        convert_prompt_to_audio();
-                        
-                        merge_video();
-
-                        add_subs();
-                    }
-                    
-               }
+                // Generate video!
+                convert_prompt_to_audio();
+                merge_video();
+                add_subs(filename);
+                
+                // Make sure to free to prevent memory leaks
+                free(filename);
             }
         }
-
+        num_lines++;
     }
+
     free(IN_VIDEO);
+    fclose(file_ptr);
     return 0;
 }
+
